d670cc9a51911ba563ce9150b4382ff140c16166
utashih <utashih@gmail.com>
2018-06-23 15:13:21 +0800

    add API impl for debug use

--- src/API/API.cpp
+++ src/API/API.cpp
@@ -5,7 +5,9 @@ void API::createTable(const std::string &tableName,
                       const std::string &primaryKey,
                       const std::vector<Attribute> &attributes) {
     std::cout << "create table " << tableName << " (" << std::endl;
-    for (auto &attr : attributes) {
+
+    for (int i = 0; i < attributes.size(); i++) {
+        auto &attr = attributes[i];
         std::cout << "    " << attr.attrName;
         switch (attr.attrType) {
         case AttrType::INT: {
@@ -24,9 +26,11 @@ void API::createTable(const std::string &tableName,
         if (attr.isUnique) {
             std::cout << " unique";
         }
+
         std::cout << ",\n";
     }
-    std::cout << ");\n";
+    std::cout << "    primary key (" << primaryKey << ")";
+    std::cout << "\n);\n";
     std::cout << std::endl;
 }
 
@@ -55,15 +59,43 @@ void API::select(const std::vector<std::string> &attributes,
     if (attributes.empty()) {
         std::cout << "* ";
     } else {
-        for (auto &name : attributes)
-            std::cout << name << ", ";
+        for (int i = 0; i < attributes.size(); i++) {
+            if (i)
+                std::cout << ", ";
+            std::cout << attributes[i];
+        }
     }
     std::cout << '\n';
     std::cout << "from " << tableName;
     if (!predicates.empty()) {
         std::cout << "\nwhere ";
-        for (auto pred : predicates) {
-            std::cout << pred.attrName << " and ";
+        for (int i = 0; i < predicates.size(); i++) {
+            if (i)
+                std::cout << " and ";
+            auto &pred = predicates[i];
+            std::cout << pred.attrName;
+
+            if (pred.op == OpType::EQ) {
+                std::cout << " = ";
+            } else if (pred.op == OpType::NE) {
+                std::cout << " <> ";
+            } else if (pred.op == OpType::LT) {
+                std::cout << " < ";
+            } else if (pred.op == OpType::LEQ) {
+                std::cout << " <= ";
+            } else if (pred.op == OpType::GT) {
+                std::cout << " > ";
+            } else if (pred.op == OpType::GEQ) {
+                std::cout << " >= ";
+            }
+
+            if (pred.val.attrType == AttrType::INT) {
+                std::cout << pred.val.ival;
+            } else if (pred.val.attrType == AttrType::FLOAT) {
+                std::cout << pred.val.fval;
+            } else if (pred.val.attrType == AttrType::CHAR) {
+                std::cout << "\'" << pred.val.cval << "\'";
+            }
         }
     }
     std::cout << ";" << std::endl;
@@ -73,21 +105,64 @@ void API::select(const std::vector<std::string> &attributes,
 void API::insert(const std::string &tableName,
                  const std::vector<Value> &values) {
     std::cout << "insert into " << tableName << std::endl;
+    std::cout << "values (";
+    for (int i = 0; i < values.size(); i++) {
+        if (i)
+            std::cout << ", ";
+        auto &value = values[i];
+        if (value.attrType == AttrType::INT) {
+            std::cout << value.ival;
+        } else if (value.attrType == AttrType::FLOAT) {
+            std::cout << value.fval;
+        } else if (value.attrType == AttrType::CHAR) {
+            std::cout << "\'" << value.cval << "\'";
+        }
+    }
+    std::cout << ");\n";
     std::cout << std::endl;
 }
 
 void API::deleteFrom(const std::string &tableName,
                      const std::vector<Predicate> &predicates) {
-    std::cout << "delete from " << tableName << std::endl;
+    std::cout << "delete ";
+    std::cout << "from " << tableName;
+    if (!predicates.empty()) {
+        std::cout << "\nwhere ";
+        for (int i = 0; i < predicates.size(); i++) {
+            if (i)
+                std::cout << " and ";
+            auto &pred = predicates[i];
+            std::cout << pred.attrName;
+
+            if (pred.op == OpType::EQ) {
+                std::cout << " = ";
+            } else if (pred.op == OpType::NE) {
+                std::cout << " <> ";
+            } else if (pred.op == OpType::LT) {
+                std::cout << " < ";
+            } else if (pred.op == OpType::LEQ) {
+                std::cout << " <= ";
+            } else if (pred.op == OpType::GT) {
+                std::cout << " > ";
+            } else if (pred.op == OpType::GEQ) {
+                std::cout << " >= ";
+            }
+
+            if (pred.val.attrType == AttrType::INT) {
+                std::cout << pred.val.ival;
+            } else if (pred.val.attrType == AttrType::FLOAT) {
+                std::cout << pred.val.fval;
+            } else if (pred.val.attrType == AttrType::CHAR) {
+                std::cout << "\'" << pred.val.cval << "\'";
+            }
+        }
+    }
+    std::cout << ";" << std::endl;
     std::cout << std::endl;
 }
 
 void API::quit() {
-    std::cout << "quit;" << std::endl;
-    std::cout << std::endl;
-}
-
-void API::execfile(const std::string &filePath) {
-    std::cout << "execfile [" << filePath << "];" << std::endl;
+    std::cout << "quiting..." << std::endl;
+    exit(0);
     std::cout << std::endl;
 }
