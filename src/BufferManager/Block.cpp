325bebdf1bff57fbd2501a06e3d5133597855440
utashih <utashih@gmail.com>
2018-06-24 23:17:31 +0800

    fix IO bug

--- src/BufferManager/Block.cpp
+++ src/BufferManager/Block.cpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <cstring>
 #include <fstream>
+using namespace std;
 
 namespace BufferManager {
 
@@ -12,7 +13,7 @@ Block::Block(const BlockID &id)
 
 void Block::read() {
     std::ifstream ifs;
-    ifs.open(filename, std::ios::binary);
+    ifs.open(filename, std::ios::in | std::ios::binary);
     ifs.seekg(BLOCK_SIZE * offset, std::ios::beg);
     ifs.read(block_data, BLOCK_SIZE);
     ifs.close();
@@ -20,9 +21,10 @@ void Block::read() {
 
 void Block::write() {
     std::ofstream ofs;
-    ofs.open(filename, std::ios::binary);
+    ofs.open(filename, std::ios::out | std::ios::binary);
     ofs.seekp(BLOCK_SIZE * offset, std::ios::beg);
     ofs.write(block_data, BLOCK_SIZE);
+    ofs.flush();
     ofs.close();
 }
 
