c43997f8a7f637f7d491a871466822dda7e42f51
utashih <utashih@gmail.com>
2018-06-26 15:44:14 +0800

    support selection with predicates & projection

--- include/RecordManager/RecordManager.h
+++ include/RecordManager/RecordManager.h
@@ -16,6 +16,11 @@ void createTable(const std::string &);
 void dropTable(const std::string &);
 void insertRecord(const std::string &, const Record &);
 void deleteAllRecords(const std::string &);
-std::vector<Record> loadTable(std::shared_ptr<Schema>);
+void deleteRecords(const std::string &, const std::vector<uint32_t> &);
+std::vector<Record> selectRecords(std::shared_ptr<Schema>,
+                                  const std::vector<Predicate> &);
+std::vector<Record> project(const std::vector<Record> &,
+                            std::shared_ptr<Schema>,
+                            const std::vector<std::string> &);
 
 } // namespace RecordManager
--- src/RecordManager/RecordManager.cpp
+++ src/RecordManager/RecordManager.cpp
@@ -9,6 +9,31 @@ namespace BM = BufferManager;
 
 void init() {}
 
+static bool satisfy(std::shared_ptr<Schema> schema, const Predicate &predicate,
+                    const Record &record) {
+    for (int i = 0; i < record.size(); i++) {
+        auto &attribute = schema->attributes[i];
+        if (predicate.attrName == attribute.name) {
+            auto &value = record[i];
+            switch (predicate.op) {
+            case OpType::EQ:
+                return value == predicate.val;
+            case OpType::NE:
+                return value != predicate.val;
+            case OpType::LT:
+                return value < predicate.val;
+            case OpType::LEQ:
+                return value <= predicate.val;
+            case OpType::GT:
+                return value > predicate.val;
+            case OpType::GEQ:
+                return value >= predicate.val;
+            }
+        }
+    }
+    throw SQLError("cannot find attribute \'" + predicate.attrName + "\'");
+}
+
 void createTable(const std::string &tableName) {
     auto filename = File::tableFilename(tableName);
     if (!BM::fileExists(filename)) {
@@ -41,87 +66,43 @@ void insertRecord(const std::string &tableName, const Record &record) {
         throw std::runtime_error("file type not compatible");
     }
 
-    if (header.deletedOffset != 0) {
-        uint32_t newPos = header.deletedOffset;
-        uint32_t blkOff = BM::blockOffset(newPos);
-        uint32_t inBlkOff = BM::inBlockOffset(newPos);
-        BM::PtrBlock blk = BM::readBlock(BM::makeID(filename, blkOff));
-        blk->resetPos(inBlkOff);
-        uint32_t newDelOff;
-        blk->read(reinterpret_cast<char *>(&newDelOff), sizeof(uint32_t));
+    uint32_t newPos = header.availableOffset;
+    uint32_t blkOff = BM::blockOffset(newPos);
+    uint32_t inBlkOff = BM::inBlockOffset(newPos);
+    BM::PtrBlock blk = BM::readBlock(BM::makeID(filename, blkOff));
+    blk->resetPos(inBlkOff);
 
-        uint32_t _offset = inBlkOff;
-        auto write = [&](const char *src, size_t size) {
-            BM::writeBlock(BM::makeID(filename, blkOff), src, _offset, size);
-            _offset += size;
-        };
-        write(reinterpret_cast<const char *>(&header.beginOffset),
-              sizeof(uint32_t));
-        if (record.size() != schema->attributes.size()) {
-            throw SQLError("value size mismatch");
-        }
-        for (int i = 0; i < record.size(); i++) {
-            if (record[i].type != schema->attributes[i].type) {
-                throw SQLError("value type mismatch");
-            }
-            if (record[i].type == ValueType::CHAR &&
-                record[i].size() > schema->attributes[i].size()) {
-                throw SQLError("string " + record[i].toString() +
-                               " is too long to fit in char(" +
-                               std::to_string(schema->attributes[i].size()) +
-                               ")");
-            }
-            write(record[i].val(), schema->attributes[i].size());
-        }
-        header.beginOffset = newPos;
-        header.deletedOffset = newDelOff;
-        BM::writeBlock(BM::makeID(filename, 0),
-                       reinterpret_cast<const char *>(&header), 0,
-                       sizeof(header));
-        return;
-    } else {
-        // no space marked as deleted
-        // allocate new space
-        uint32_t newPos = header.availableOffset;
-        uint32_t blkOff = BM::blockOffset(newPos);
-        uint32_t inBlkOff = BM::inBlockOffset(newPos);
-        BM::PtrBlock blk = BM::readBlock(BM::makeID(filename, blkOff));
-        blk->resetPos(inBlkOff);
-
-        uint32_t _offset = inBlkOff;
-        auto write = [&](const char *src, size_t size) {
-            BM::writeBlock(BM::makeID(filename, blkOff), src, _offset, size);
-            _offset += size;
-        };
-        write(reinterpret_cast<const char *>(&header.beginOffset),
-              sizeof(uint32_t));
-        if (record.size() != schema->attributes.size()) {
-            throw SQLError("value size mismatch");
-        }
-        for (int i = 0; i < record.size(); i++) {
-            if (record[i].type != schema->attributes[i].type) {
-                throw SQLError("value type mismatch");
-            }
-            if (record[i].type == ValueType::CHAR &&
-                record[i].size() > schema->attributes[i].size()) {
-                throw SQLError("string " + record[i].toString() +
-                               " is too long to fit in char(" +
-                               std::to_string(schema->attributes[i].size()) +
-                               ")");
-            }
-            write(record[i].val(), schema->attributes[i].size());
-        }
-        header.beginOffset = newPos;
-        uint32_t size = recordBinarySize(*schema);
-        if (inBlkOff + size >= BM::BLOCK_SIZE) {
-            header.availableOffset = header.blockNum++ * BM::BLOCK_SIZE;
-        } else {
-            header.availableOffset = newPos + size;
-        }
-        BM::writeBlock(BM::makeID(filename, 0),
-                       reinterpret_cast<const char *>(&header), 0,
-                       sizeof(header));
+    uint32_t _offset = inBlkOff;
+    auto write = [&](const char *src, size_t size) {
+        BM::writeBlock(BM::makeID(filename, blkOff), src, _offset, size);
+        _offset += size;
+    };
+    write(reinterpret_cast<const char *>(&header.beginOffset),
+          sizeof(uint32_t));
+    if (record.size() != schema->attributes.size()) {
+        throw SQLError("value size mismatch");
     }
+    for (int i = 0; i < record.size(); i++) {
+        if (record[i].type != schema->attributes[i].type) {
+            throw SQLError("value type mismatch");
+        }
+        if (record[i].type == ValueType::CHAR &&
+            record[i].size() > schema->attributes[i].size()) {
+            throw SQLError("string " + record[i].toString() +
+                           " is too long to fit in char(" +
+                           std::to_string(schema->attributes[i].size()) + ")");
+        }
+        write(record[i].val(), schema->attributes[i].size());
+    }
+    header.beginOffset = newPos;
+    uint32_t size = recordBinarySize(*schema);
+    if (inBlkOff + size >= BM::BLOCK_SIZE) {
+        header.availableOffset = header.blockNum++ * BM::BLOCK_SIZE;
+    } else {
+        header.availableOffset = newPos + size;
+    }
+    BM::writeBlock(BM::makeID(filename, 0),
+                   reinterpret_cast<const char *>(&header), 0, sizeof(header));
 }
 
 void deleteAllRecords(const std::string &tableName) {
@@ -138,13 +119,43 @@ void deleteAllRecords(const std::string &tableName) {
     }
     header.blockNum = 1;
     header.beginOffset = 0;
-    header.deletedOffset = 0;
     header.availableOffset = BM::BLOCK_SIZE;
     BM::writeBlock(BM::makeID(filename, 0),
                    reinterpret_cast<const char *>(&header), 0, sizeof(header));
 }
 
-std::vector<Record> loadTable(std::shared_ptr<Schema> schema) {
+void deleteRecords(const std::string &tableName,
+                   const std::vector<uint32_t> &offsets) {
+    auto filename = File::tableFilename(tableName);
+    if (!BM::fileExists(filename)) {
+        throw SQLError("table \'" + tableName + "\' not exist");
+    }
+    BM::PtrBlock blk0 = BM::readBlock(BM::makeID(filename, 0));
+    blk0->resetPos();
+    File::tableFileHeader header;
+    blk0->read(reinterpret_cast<char *>(&header), sizeof(header));
+    if (header.filetype != static_cast<uint32_t>(File::FileType::TABLE)) {
+        throw std::runtime_error("file type not compatible");
+    }
+    for (uint32_t pos : offsets) {
+        uint32_t blkOff = BM::blockOffset(pos);
+        uint32_t inBlkOff = BM::inBlockOffset(pos);
+        BM::PtrBlock blk = BM::readBlock(BM::makeID(filename, blkOff));
+        blk->resetPos(inBlkOff);
+        uint32_t mark;
+        blk->read(reinterpret_cast<char *>(&mark), sizeof(uint32_t));
+        if (mark & DELETED_MARK) {
+            throw SQLError("record has already been deleted");
+        }
+        mark |= DELETED_MARK;
+        BM::writeBlock(BM::makeID(filename, blkOff),
+                       reinterpret_cast<const char *>(&mark), inBlkOff,
+                       sizeof(uint32_t));
+    }
+}
+
+std::vector<Record> selectRecords(std::shared_ptr<Schema> schema,
+                                  const std::vector<Predicate> &predicates) {
     auto &tableName = schema->tableName;
     std::vector<Record> records;
     auto filename = File::tableFilename(tableName);
@@ -166,6 +177,10 @@ std::vector<Record> loadTable(std::shared_ptr<Schema> schema) {
         blk->resetPos(inBlkOff);
         Record record;
         blk->read(reinterpret_cast<char *>(&pos), sizeof(uint32_t));
+        if (pos & DELETED_MARK) {
+            pos &= DELETED_MASK;
+            continue;
+        }
         for (auto &attribute : schema->attributes) {
             Value value(attribute);
             if (value.type == ValueType::CHAR) {
@@ -174,9 +189,48 @@ std::vector<Record> loadTable(std::shared_ptr<Schema> schema) {
             blk->read(value.val(), value.size());
             record.emplace_back(std::move(value));
         }
-        records.push_back(record);
+        bool chosen = true;
+        for (auto predicate : predicates) {
+            chosen &= satisfy(schema, predicate, record);
+            if (!chosen) {
+                break;
+            }
+        }
+        if (chosen) {
+            records.push_back(record);
+        }
     }
     return records;
+}
+
+std::vector<Record> project(const std::vector<Record> &records,
+                            std::shared_ptr<Schema> schema,
+                            const std::vector<std::string> &attributes) {
+    if (attributes.empty()) {
+        return records;
+    }
+    std::vector<int> permutation;
+    for (auto &attrName : attributes) {
+        bool found = false;
+        for (int i = 0; !found && i < schema->attributes.size(); i++) {
+            if (attrName == schema->attributes[i].name) {
+                permutation.push_back(i);
+                found = true;
+            }
+        }
+        if (!found) {
+            throw SQLError("cannot find attribute \'" + attrName + "\'");
+        }
+    }
+    std::vector<Record> results;
+    for (auto &record : records) {
+        Record projected;
+        for (int pos : permutation) {
+            projected.emplace_back(record[pos]);
+        }
+        results.emplace_back(projected);
+    }
+    return results;
 }
 
 void exit() {}
