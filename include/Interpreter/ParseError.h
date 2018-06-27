13cb657c6d6f384252754d8a717cb8936dbb79ee
utashih <utashih@gmail.com>
2018-06-28 01:01:29 +0800

    fix bugs outputing parse error msg

--- include/Interpreter/ParseError.h
+++ include/Interpreter/ParseError.h
@@ -9,9 +9,5 @@ enum class end_of_file { eof };
 class ParseError : public std::runtime_error {
   public:
     explicit ParseError(const std::string &, const int nl, const int nc);
-    const char *what() const noexcept;
-
-  private:
-    int nl, nc;
 };
 } // namespace Interpreter
--- src/Interpreter/ParseError.cpp
+++ src/Interpreter/ParseError.cpp
@@ -3,16 +3,19 @@
 
 namespace Interpreter {
 
-ParseError::ParseError(const std::string &what_arg, const int nl, const int nc)
-    : std::runtime_error(what_arg), nl(nl), nc(nc) {}
-
-const char *ParseError::what() const noexcept {
+static std::string errMsg(const std::string &what_arg, const int nl,
+                          const int nc) {
     std::stringstream ss;
     if (nl != -1 && nc != -1) {
         ss << "Line " << nl << ", Column " << nc << ":" << std::endl;
+    } else {
+        ss << "At the end of input:" << std::endl;
     }
-    ss << "\033[31mParseError: \033[0m" << std::runtime_error::what();
-    return ss.str().c_str();
+    ss << "\033[31mParseError: \033[0m" << what_arg;
+    return ss.str();
 }
+
+ParseError::ParseError(const std::string &what_arg, const int nl, const int nc)
+    : std::runtime_error(errMsg(what_arg, nl, nc)) {}
 
 } // namespace Interpreter
