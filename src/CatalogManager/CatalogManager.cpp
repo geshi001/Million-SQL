d446e84fd2692526bb171f0e3e34c115af22f8c6
utashih <utashih@gmail.com>
2018-06-28 12:27:48 +0800

    implement create/drop index in CM

--- src/CatalogManager/CatalogManager.cpp
+++ src/CatalogManager/CatalogManager.cpp
@@ -1,10 +1,12 @@
 #include <BufferManager/Block.h>
 #include <CatalogManager/CatalogManager.h>
 #include <CatalogManager/TableSpec.h>
+#include <IndexManager/IndexManager.h>
+#include <RecordManager/RecordManager.h>
+#include <SysError.h>
 #include <algorithm>
 #include <list>
 #include <memory>
-#include <stdexcept>
 #include <string>
 #include <unordered_map>
 
@@ -13,13 +15,23 @@ namespace CM {
 std::unordered_map<std::string, std::shared_ptr<Schema>> mapSchemas;
 std::unordered_map<std::string, uint32_t> mapSchemaOffsets;
 
+std::unordered_map<std::string, Index> mapIndices;
+std::unordered_map<std::string, uint32_t> mapIndexOffsets;
+
 bool hasTable(const std::string &tableName) {
     return mapSchemas.find(tableName) != mapSchemas.end();
+}
+
+bool hasIndex(const std::string &indexName) {
+    return mapIndices.find(indexName) != mapIndices.end();
 }
 
 void init() {
     mapSchemas.clear();
     mapSchemaOffsets.clear();
+
+    mapIndices.clear();
+    mapIndexOffsets.clear();
 
     auto filename = File::catalogFilename();
     if (!BM::fileExists(filename)) {
@@ -32,6 +44,7 @@ void init() {
 
     uint32_t currP = header.tableOffset, nextP = 0;
     static char strbuf[NAME_LENGTH];
+
     while (currP != 0) {
         BM::PtrBlock blk = BM::readBlock(BM::makeID(filename, currP));
         blk->resetPos();
@@ -57,8 +70,38 @@ void init() {
                 decodeProperties(bin);
             schema->attributes.push_back(attribute);
         }
+        if (!RM::hasTable(schema->tableName)) {
+            throw SysError("missing data for table \'" + schema->tableName +
+                           "\'");
+        }
         mapSchemas[schema->tableName] = schema;
         mapSchemaOffsets[schema->tableName] = currP;
+        currP = nextP;
+    }
+
+    currP = header.indexOffset;
+    nextP = 0;
+    while (currP != 0) {
+        BM::PtrBlock blk = BM::readBlock(BM::makeID(filename, currP));
+        blk->resetPos();
+        blk->read(reinterpret_cast<char *>(&nextP), sizeof(uint32_t));
+        if (nextP & DELETED_MARK) {
+            currP = nextP & DELETED_MASK;
+            continue;
+        }
+        Index index;
+        blk->read(strbuf, NAME_LENGTH);
+        index.indexName = std::string(strbuf);
+        blk->read(strbuf, NAME_LENGTH);
+        index.tableName = std::string(strbuf);
+        blk->read(strbuf, NAME_LENGTH);
+        index.attrName = std::string(strbuf);
+        if (!IM::hasTable(index.indexName)) {
+            throw SysError("missing data for index \'" + index.indexName +
+                           "\'");
+        }
+        mapIndices[index.indexName] = index;
+        mapIndexOffsets[index.indexName] = currP;
         currP = nextP;
     }
 }
@@ -123,7 +166,6 @@ void dropTable(const std::string &tableName) {
         throw SQLError("table \'" + tableName + "\' does not exist");
     }
     auto filename = File::catalogFilename();
-    auto schema = mapSchemas[tableName];
     uint32_t offset = mapSchemaOffsets[tableName], nextP;
 
     BM::PtrBlock blk = BM::readBlock(BM::makeID(filename, offset));
@@ -138,11 +180,68 @@ void dropTable(const std::string &tableName) {
 
 void createIndex(const std::string &indexName, const std::string &tableName,
                  const std::string &attrName) {
-    //
+    if (hasIndex(indexName)) {
+        throw SQLError("index \'" + indexName + "\' already exists");
+    }
+    Index index;
+    index.indexName = indexName;
+    index.tableName = tableName;
+    index.attrName = attrName;
+    mapIndices[indexName] = index;
+
+    auto filename = File::catalogFilename();
+    BM::PtrBlock blk0 = BM::readBlock(BM::makeID(filename, 0));
+    File::catalogFileHeader header;
+    blk0->resetPos();
+    blk0->read(reinterpret_cast<char *>(&header), sizeof(header));
+
+    uint32_t newP = header.blockNum++;
+    uint32_t nextP = header.indexOffset;
+    header.indexOffset = newP;
+    mapIndexOffsets[indexName] = newP;
+
+    uint32_t offset0 = 0, offsetP = 0;
+    auto write0 = [&](const char *src, size_t size) {
+        BM::writeBlock(BM::makeID(filename, 0), src, offset0, size);
+        offset0 += size;
+    };
+    auto writeP = [&](const char *src, size_t size) {
+        BM::writeBlock(BM::makeID(filename, newP), src, offsetP, size);
+        offsetP += size;
+    };
+
+    write0(reinterpret_cast<const char *>(&header), sizeof(header));
+    writeP(reinterpret_cast<const char *>(&nextP), sizeof(uint32_t));
+    static char strbuf[NAME_LENGTH];
+
+    memset(strbuf, 0, NAME_LENGTH);
+    std::strcpy(strbuf, indexName.c_str());
+    writeP(strbuf, NAME_LENGTH);
+
+    memset(strbuf, 0, NAME_LENGTH);
+    std::strcpy(strbuf, tableName.c_str());
+    writeP(strbuf, NAME_LENGTH);
+
+    memset(strbuf, 0, NAME_LENGTH);
+    std::strcpy(strbuf, attrName.c_str());
+    writeP(strbuf, NAME_LENGTH);
 }
 
 void dropIndex(const std::string &indexName) {
-    //
+    if (!hasIndex(indexName)) {
+        throw SQLError("index \'" + indexName + "\' does not exist");
+    }
+    auto filename = File::catalogFilename();
+    uint32_t offset = mapIndexOffsets[indexName], nextP;
+
+    BM::PtrBlock blk = BM::readBlock(BM::makeID(filename, offset));
+    blk->resetPos(0);
+    blk->read(reinterpret_cast<char *>(&nextP), sizeof(uint32_t));
+    nextP |= DELETED_MARK;
+    BM::writeBlock(BM::makeID(filename, offset),
+                   reinterpret_cast<const char *>(&nextP), 0, sizeof(uint32_t));
+    mapIndices.erase(indexName);
+    mapIndexOffsets.erase(indexName);
 }
 
 void exit() {}
